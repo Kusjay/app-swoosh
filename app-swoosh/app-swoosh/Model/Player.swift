f728987208b13933b601f2ed8d040de4b0c86d23
Kusjay <kelechiuma@gmail.com>
2018-11-11 10:27:51 +0100

    ibactions and data models

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Kusjay on 11/9/18.
+//  Copyright © 2018 Kusjay. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
